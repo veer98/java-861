89c6d5ae96efc0d5cd4dd5b9b4927d99b3b1480d
veer98 <51856799+veer98@users.noreply.github.com>
2020-10-15 12:41:15 +0530

    added employee class

---
+++ EmployeeManagement/src/com/lti/model/Employee.java
@@ -0,0 +1,29 @@
+package com.lti.model;
+
+public class Employee {
+	
+	private int employeeId;
+	private String employeeName;
+	
+	public Employee()
+	{
+		
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+}
